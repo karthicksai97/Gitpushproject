a36d40a5eb6df31620eaa27cd919d1439df062ad
Karthicksai97 <karthicksai97@gamil.com>
2022-07-09 20:32:07 +0530

    project move to git

---
+++ src/org/push/Git_push_class.java
@@ -0,0 +1,12 @@
+package org.push;
+
+public class Git_push_class {
+
+	private void Gitday1() {
+		System.out.println("Project 1");
+	}
+	
+	private void Gitday2() {
+		System.out.println("Project 2");
+	}
+}
